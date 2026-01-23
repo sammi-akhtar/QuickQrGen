6a84bfe8eb9ebfbf436576f3aa628ec17b53f2f9
SAMMI AKHTAR <125820612+sammi-akhtar@users.noreply.github.com>
2026-01-23 14:14:51 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
+
+
+
+
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10497178
+    "zoneId": 10505892
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
