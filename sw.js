abcdf5f266d07adb7bbf9148777fb7483c1d3c5d
SAMMI AKHTAR <125820612+sammi-akhtar@users.noreply.github.com>
2026-03-24 18:30:45 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@
 
 
 
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10505892
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+// self.options = {
+//     "domain": "3nbf4.com",
+//     "zoneId": 10505892
+// }
+// self.lary = ""
+// importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
